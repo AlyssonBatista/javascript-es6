4860bfe43325f5d3dae5326c6d7ab5ac9dc9d3a9
Alysson Batista <alyssonbatista241@gmail.com>
2021-10-10 17:16:54 -0300

    .

---
+++ variaveis/exercicio.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+
+var nome = 'Alyssono';
+
+// Declarar uma variável com a sua idade
+
+var idade = '30';
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comida;
+
+// Atribuir valor a sua comida favorita
+
+comida = 'lasanha';
+
+// Declarar 5 variáveis diferentes sem valore
+var RJ,BA,AL,AM,SE;
+
+console.log(nome, idade, comida, RJ = 45);
---
+++ variaveis/variaveis.js
@@ -0,0 +1,19 @@
+var nome = 'Alysson'; // valor variável
+let idade = 30;// valor variavel*
+const possuiFaculdade = true; // valor constante nunca é alterado
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+var preco = 20; 
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+// criando mais de uma variável com apenas um var 
+
+var nome = 'alysson',
+    idade = 30,
+    possuiFaculdade = 'false';
+
+
+
+
